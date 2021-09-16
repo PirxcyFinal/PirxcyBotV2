3171afa4fffe0f3f6e112e130e0f04144715d222
Zy (Pirxcy) <71592833+PirxcyFinal@users.noreply.github.com>
2021-09-16 06:56:02 +0100

    Update main.py

--- source/main.py
+++ source/main.py
@@ -25,7 +25,7 @@ Gomashio or PDF if you see this keep up the great work <3
 I Spent Alot Of Time On This Bot So if You Could Support Me By Joining I would appreciate it!
     - Pirxcy
 """ 
-
+import PirxcyPinger
 import os
 import fortnitepy
 from fortnitepy.ext import commands
@@ -87,10 +87,8 @@ def intro():
 loop = asyncio.get_event_loop()
 
 
-def keep_alive():
-    url = f'https://{os.getenv("REPL_SLUG")}.{os.getenv("REPL_OWNER")}.repl.co'
-    requests.post('https://pinger.pirxcy.xyz/api/add', json={'url': url})
-    print("Posted URL To PirxcyPinger!")
+async def keep_alive():
+  await PirxcyPinger.post(f"https://{os.environ['REPL_ID']}.id.repl.co")
     
 def reset_config():
     try:
@@ -125,205 +123,15 @@ with open('settings.json') as f:
       reset_config()
       exit(1)
 
-
-#Credit AtomicXYZ For The Code SimpleDeviceAuthHandler
-#https://github.com/AtomicXYZ/EpicEndpoints/
-
-class SimpleDeviceAuthHandler:
-
-    def __init__(self) -> None:
-        headers = {
-            'content-type': "application/x-www-form-urlencoded",
-            'authorization': "basic YjA3MGYyMDcyOWY4NDY5M2I1ZDYyMWM5MDRmYzViYzI6SEdAWEUmVEdDeEVKc2dUIyZfcDJdPWFSbyN+Pj0+K2M2UGhSKXpYUA==",
-        }
-        response = requests.request("POST","https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token", data="grant_type=client_credentials", headers=headers)
-        self.access_token = json.loads(response.text)['access_token']
-        self.device_code = None
-
-    def login(self):
-        url2 = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization"
-
-        querystring2 = {"prompt":"login"}
-
-        payload2 = "prompt=promptType"
-        headers2 = {
-            'content-type': "application/x-www-form-urlencoded",
-            'authorization': f"bearer {self.access_token}",
-            }
-
-        response2 = requests.request("POST", url2, data=payload2, headers=headers2, params=querystring2)
-
-        url = response2.json()['verification_uri_complete']
-        self.device_code = response2.json()['device_code']
-
-        return url
-
-    def device_auth(self):
-        device_auths = {}
-
-        if self.device_code == None:
-            return {}
-        else:
-            try:
-                deviceCode = self.device_code
-                clientToken = "NTIyOWRjZDNhYzM4NDUyMDhiNDk2NjQ5MDkyZjI1MWI6ZTNiZDJkM2UtYmY4Yy00ODU3LTllN2QtZjNkOTQ3ZDIyMGM3"
-                url3 = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
-                payload3 = f"grant_type=device_code&device_code={deviceCode}"
-                headers3 = {
-                    'content-type': "application/x-www-form-urlencoded",
-                    'authorization': f"basic {clientToken}",
-                    }
-                status = 0
-                time_start = time.time()
-                while(status != 200):
-                    response3 = requests.request("POST", url3, data=payload3, headers=headers3)
-                    status = response3.status_code
-                    time_now = time.time()
-                    if(time_now-time_start >= 600):
-                        return {}
-                    time.sleep(1)
-                self.device_code = None
-                data = response3.json()
-
-                device_auths['display_name'] = data['displayName']
-
-                account_id = data['account_id']
-                access_token = data['access_token']
-
-                url = f"https://account-public-service-prod.ol.epicgames.com/account/api/public/account/{account_id}/deviceAuth"
-                headers = {
-                    'content-type': "application/json",
-                    'authorization': f"bearer {access_token}",
-                    }
-                response = requests.request("POST", url, headers=headers)
-                device_details = response.json()
-
-                device_auths['device_id'] = device_details['deviceId']
-                device_auths['account_id'] = device_details['accountId']
-                device_auths['secret'] = device_details['secret']
-
-                return device_auths
-            except:
-                return {}
-
-def getDisplayName(account_id, access_token):
-  url = "https://account-public-service-prod.ol.epicgames.com/account/api/public/account/"
-
-  querystring2 = {"accountId":account_id}
-
-  headers = {
-        'content-type': "application/json",
-        'authorization': f"bearer {access_token}",
-        }
-
-  response = requests.request("GET", url, headers=headers,params=querystring2)
-
-  data = json.loads(response.text)
-  print(data)
-  return data[0]['displayName']
-
-def getAccessToken(clientId, secret):
-    id = clientId + ":" + secret
-    clientToken = base64.b64encode(bytes(id, 'utf-8'))
-    clientToken = clientToken.decode("utf-8")
-
-    url = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
-
-    payload = "grant_type=client_credentials"
-    headers = {
-        'content-type': "application/x-www-form-urlencoded",
-        'authorization': f"basic {clientToken}",
-        }
-
-    response = requests.request("POST", url, data=payload, headers=headers)
-
-    data = json.loads(response.text)
-
-    access_token = data['access_token']
-    expire = data['expires_in']
-
-    return access_token
-
-def getDeviceCode(access_token):
-    url2 = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/deviceAuthorization"
-
-    querystring2 = {"prompt":"login"}
-
-    payload2 = "prompt=promptType"
-    headers2 = {
-        'content-type': "application/x-www-form-urlencoded",
-        'authorization': f"bearer {access_token}",
-        }
-
-    response2 = requests.request("POST", url2, data=payload2, headers=headers2, params=querystring2)
-
-    data2 = json.loads(response2.text)
-    print(data2)
-
-    return data2
-
-def deviceCodeData(deviceCode, clientId, secret):
-
-    id = clientId + ":" + secret
-    clientToken = base64.b64encode(bytes(id, 'utf-8'))
-    clientToken = clientToken.decode("utf-8")
-
-    url3 = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
-
-    payload3 = f"grant_type=device_code&device_code={deviceCode}"
-    headers3 = {
-        'content-type': "application/x-www-form-urlencoded",
-        'authorization': f"basic {clientToken}",
-        }
-
-    response3 = requests.request("POST", url3, data=payload3, headers=headers3)
-
-    data3 = json.loads(response3.text)
-
-    return data3
-
-def getDeviceAuth(access_token,account_id):
-  url = f"https://account-public-service-prod.ol.epicgames.com/account/api/public/account/{account_id}/deviceAuth"
-
-  headers = {
-      'content-type': "application/json",
-      'authorization': f"bearer {access_token}",
-      }
-
-  response = requests.request("POST", url, headers=headers)
-
-  data = json.loads(response.text)
-
-  return data
-
 #I would Make it Async But Thats wayyy To Much Effort For Me.
 #Lesson From Pirxcy Take What You Are Given With.
 
-#Login
-handler = SimpleDeviceAuthHandler() #Handler
-loginurl = handler.login() #URL To Sign in With
-intro()
-print("Sign in With\n" + loginurl)
-device_auth_details = handler.device_auth() #Script Continues Once They Have Logged in!
-device = device_auth_details['device_id']
-account = device_auth_details['account_id']
-secret = device_auth_details['secret']
-with open("settings.json", "r+") as file:
-  datas = json.load(file)
-  datas.update({"device_id": device})
-  datas.update({"account_id": account})
-  datas.update({"secret": secret})
-  file.seek(0)
-  json.dump(datas, file, indent=2)
-  file.close()
 
 client = commands.Bot(
 	command_prefix=data['prefix'],
-	auth=fortnitepy.DeviceAuth(
-	device_id=device,
-	account_id=account,
-	secret=secret,
-	ios_token="NTIyOWRjZDNhYzM4NDUyMDhiNDk2NjQ5MDkyZjI1MWI6ZTNiZDJkM2UtYmY4Yy00ODU3LTllN2QtZjNkOTQ3ZDIyMGM3"
+	auth=fortnitepy.AuthorizationCodeAuth(
+  intro(),
+  code = input('Enter Code!\n')
 	)
 )
 
@@ -477,9 +285,7 @@ async def event_ready():
 			debug=True,
 			backlog=100
 	)
-	print("Connecting To Pinger")
-	keep_alive()
-	print("Connected")
+	await keep_alive()
 	if data['status']:
 			await client.set_presence(data['status']) 
 	print(f'Launched {client.user.display_name}\nConnecting To Dashboard')
